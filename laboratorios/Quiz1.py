2e8e95200b012af205c14ab905a19f9d4d713be4
Arquimedes Escartín <a.j.en919@gmail.com>
2015-09-21 21:54:47 -0500

    Create Quiz1.py

---
+++ laboratorios/Quiz1.py
@@ -0,0 +1,17 @@
+name = Arquimedes Escartin
+id = 8 863 235
+print("Estudiante " +name+ "y mi cedula es " +id+ " realizando el Quiz numero uno.")
+base_rec = 5
+altura_rec = 7
+area_rec = base_rec * altura_rec
+print("Calculando el area de un rectangulo, la respuesta seria " + str(area_rec) + " cms")
+perim_rec = 2 * (base_rec + altura_rec)
+print("Calculando el perimetro de un rectangulo, la respuesta seria " + str(perim_rec) + " cms")
+area_rec = (base_rec * altura_rec)/10000
+print("El area de rectangulo, convirtiendo de centimetros a metros, seria " + str(area_rec) + " metros.")
+perim_rec = (2 * (base_rec + altura_rec))/100
+print("El perimetro de rectangulo, convirtiendo de centimetros a metros, seria " + str(area_rec) + " metros.")
+area_rec = (base_rec * altura_rec)*0.39
+perim_rec = (2 * (base_rec + altura_rec))*0.39
+print("El area de rectangulo, convirtiendo de centimetros a pulgadas, seria " + str(area_rec) + " pulgadas.")
+print("El perimetro de rectangulo, convirtiendo de centimetros a pulgadas, seria " + str(perim_rec) + " pulgadas.")
