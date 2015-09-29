e959067d1df85672e18b7983b718ff4eb23ab770
Arquimedes Escartín <a.j.en919@gmail.com>
2015-09-28 21:47:41 -0500

    Create Quiz2.py

---
+++ laboratorios/Quiz2.py
@@ -0,0 +1,29 @@
+#Quiz #2
+#Almacen "El Emperador"
+#Programador y empleado del almacen El Emperador:
+#Arquimedes Escartin
+#
+#>= $500, 30% Desc
+# < $500 pero >= $200, 20% Desc
+# < $200 >= $100, 10% Desc
+
+user = 5
+x = 1
+
+while x <= user:
+	compra = int(input("Introduzca cifra: "))
+
+	if compra >= 500:
+		desc = compra * 0.3
+		print("Descuento de 30% para la compra. El total es " +str(desc)+ " dolares.")
+	
+	if compra < 500 && >= 200:
+		desc = compra * 0.2
+		print("Descuento de 20% para la compra. El total es " +str(desc)+ " dolares.")
+
+	if compra < 200 && >= 100:
+		desc = compra * 0.1
+		print("Descuento de 10% para la compra. El total es " +str(desc)+ " dolares.")
+
+	else:
+		print("El total de la compra es " +str(compra)+ " dolares. ")
